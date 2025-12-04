31c0b9a157cc6ca2434546d4dcbdf24471c6ecc3
protapkrghosh <protapg518@gmail.com>
2025-12-05 00:49:04 +0600

    update: todo service file

--- src/modules/todos/todo.controller.ts
+++ src/modules/todos/todo.controller.ts
@@ -2,10 +2,8 @@ import { Request, Response } from "express";
 import { todoServices } from "./todo.service";
 
 const createTodo = async (req: Request, res: Response) => {
-   const { user_id, title } = req.body;
-
    try {
-      const result = await todoServices.createTodo(user_id, title);
+      const result = await todoServices.createTodo(req.body);
 
       res.status(201).json({
          success: true,
--- src/modules/todos/todo.service.ts
+++ src/modules/todos/todo.service.ts
@@ -1,6 +1,8 @@
 import { pool } from "../../config/db";
 
-const createTodo = async (user_id: string, title: string) => {
+const createTodo = async (payload: Record<string, unknown>) => {
+   const { user_id, title } = payload;
+
    const result = await pool.query(
       `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
       [user_id, title]
